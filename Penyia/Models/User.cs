f377b3764ae00c113fb3974fcbe14b60bc722c27
ArunSivagnanam <Arun.s@live.dk>
2015-09-13 22:30:52 +0200

    new

---
+++ Penyia/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Penyia.Models
+{
+    public class User
+    {
+
+        public String Name { get; set; }
+
+
+
+
+
+    }
+}
