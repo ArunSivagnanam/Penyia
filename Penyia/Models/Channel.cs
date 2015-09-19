5bd9609ad52a5be812a35f5ff62f663bc350faf9
ArunSivagnanam <Arun.s@live.dk>
2015-09-19 14:18:57 +0200

    latest

---
+++ Penyia/Models/Channel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Penyia.Models
+{
+    public class Channel
+    {
+        public int ChannelID { get; set; }
+
+        // Foreign key 
+        public int UserID { get; set; }
+
+        // Navigation properties 
+        public virtual User Owner { get; set; }
+
+        public virtual ICollection<Message> ChannelMessages { get; set; } 
+
+    }
+}
