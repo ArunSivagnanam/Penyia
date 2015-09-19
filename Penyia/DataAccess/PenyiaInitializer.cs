181cec3982a688e54aa050a89be83cc23d09a47d
ArunSivagnanam <Arun.s@live.dk>
2015-09-19 14:20:33 +0200

    latest

---
+++ Penyia/DataAccess/PenyiaInitializer.cs
@@ -0,0 +1,28 @@
+﻿using Penyia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Penyia.DataAccess
+{
+    public class PenyiaInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<PenyiaContext>
+    {
+        protected override void Seed(PenyiaContext context)
+        {
+            var users = new List<User>
+            {
+            new User{FirstName="Carson",LastName="Alexander",Email = "car@live.dk",UserName = "carbo",Password = "123XCV"},
+            new User{FirstName="Arun",LastName="Sivagnanam",Email = "Arun.s@live.dk",UserName = "a-run",Password = "1234"},
+            new User{FirstName="Kim",LastName="Hansen",Email = "kimie@live.dk",UserName = "lilleKim",Password = "1234"},
+            new User{FirstName="Hans",LastName="Mongo",Email = "mongo@live.dk",UserName = "Mongo",Password = "1234"}
+          
+            };
+
+            users.ForEach(s => context.Users.Add(s));
+            context.SaveChanges();
+            
+        }
+
+    }
+}
---
+++ Penyia/Models/ChannelMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Penyia.Models
+{
+    public class ChannelMessage : Message
+    {
+        // Foreign key 
+        public int ChannelID { get; set; }
+
+        // Navigation properties 
+        public virtual Channel Channel { get; set; }
+
+    }
+}
